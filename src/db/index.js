df7d3d158ec1b70e03df23df39ea30542af73566
Rahul Nayak <nayakrahul9401@gmail.com>
2024-08-19 22:44:02 +0530

    readme added

--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ import { connect } from 'mongoose';
 import DB_NAME from '../constants.js';
 const MongooseDBconnection = async () => {
 
-    let db_url = `${process.env.MONGO_DB_URI}/${DB_NAME}`;
+    let db_url = `${process.env.MONGO_DB_URI}/${DB_NAME}`;// Replace process.env.MONGO_DB_URI with your DB_URL in .env file
     console.log(db_url);
     try {
         const instance = await connect(db_url);
